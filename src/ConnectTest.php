ca50da6d29361dc22301d7d347511bdcf6354b1b
chaconjav <chaconjav@yahoo.com>
2018-09-05 16:26:14 +0000

    Initialization

---
+++ src/ConnectTest.php
@@ -0,0 +1,16 @@
+<?php
+require_once('Connect.php');
+//class RemoteConnectTest extends PHPUnit_Framework_TestCase
+class RemoteConnectTest extends PHPUnit\Framework\TestCase
+{
+  public function setUp(){ }
+  public function tearDown(){ }
+  public function testConnectionIsValid()
+  {
+    // test to ensure that the object from an fsockopen is valid
+    $connObj = new RemoteConnect();
+    $serverName = 'www.yahoo.com';
+    $this->assertTrue($connObj->connectToServer($serverName) !== false);
+  }
+}
+?>
